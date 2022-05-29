645540d2fecc2501922df5a5e3bddacebd75707d
Wiktoria Majewska <102476053+majesia@users.noreply.github.com>
2022-05-29 12:37:56 +0200

    poprawki

--- src/Fight.java
+++ src/Fight.java
@@ -1,29 +1,54 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Fight {
-        public void fight(ArrayList<Human> humans, ArrayList<Zombie> zombies){
+        public void fight(ArrayList<Human> humans, ArrayList<Zombie> zombies, int mapSize){
             int newZombie=0;
-            Random rand = new Random();
-            for(Human human : humans)
-                for(Zombie zombie : zombies)
-                    if(human.x== zombie.x)
-                        if(human.y == zombie.y)
-                            if(zombie.combatStat > human.getCombatStat()){
-                                human.healthPoints = 0;
-                                human.humanPopulation -=1;
-                                humans.remove(human);
-                                Human.killedByZombie +=1;
-                                newZombie +=1;
-                                zombie.zombiePopulation +=1;
+            int allCombatStatsZombies = 0;
+            int allCombatStatsHumans = 0;
+            int zombieCount =0;
+            int humansCount =0;
+            ArrayList<Human> humansToRemove = new ArrayList<>();
+
+            // x=i, y=j
+            for (int i=0; i< mapSize ; i++)
+                for(int j=0; j<mapSize; j++){
+                    for(Zombie zombie : zombies)
+                        if(zombie.x == i && zombie.y==j){
+                            zombieCount +=1;
+                            allCombatStatsZombies += zombie.combatStat;
+                        }
+                    if(zombieCount !=0) {
+                        for (Human human : humans)
+                            if (human.x == i && human.y == j) {
+                                allCombatStatsHumans += human.combatStat;
+                                humansToRemove.add(human);
+                                humansCount +=1;
                             }
 
-            if(newZombie!=0){
-                for(int i=0; i<newZombie; i++) {
-                    zombies.add(new Zombie(30, rand.nextInt(100), rand.nextInt(100)));
+                        if ((allCombatStatsZombies > allCombatStatsHumans) && humansCount !=0) {
+                            for (Human human1 : humansToRemowe) {
+                                human1.healthPoints = 0;
+                                Human.humanPopulation -= 1;
+                                humans.remove(human1);
+                                Human.killedByZombie += 1;
+                                newZombie += 1;
+                            }
+
+                            if(newZombie!=0)
+                                for(int k=0; k<newZombie; k++) {
+                                    zombies.add(new Zombie(30, i, j));
+                                }
+
+                        }
+
+                        newZombie = 0;
+                        allCombatStatsHumans =0;
+                        allCombatStatsZombies =0;
+                        zombieCount =0;
+                        humansCount =0;
+                    }
+                    humansToRemove.clear();
                 }
-            }
+
         }
-
-
 }
